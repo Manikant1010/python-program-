766f14da3e57e02cb996eef983e86318296aca2f
Manikant1010 <146963843+Manikant1010@users.noreply.github.com>
2023-12-06 02:45:40 +0530

    Add files via upload

---
+++ Data_type.py
@@ -0,0 +1,10 @@
+a = "Manikant"
+b = 5
+c = .25
+print("type of a",type(a))
+print("type of b",type(b))
+print("type of c",type(c))
+
+e = int(input("integer: "))
+f = float(input("Enter float number: "))
+g = str(input("Enter String: "))
---
+++ assine_a_value.py
@@ -0,0 +1,10 @@
+# Method 1
+
+a = 1 if 20 > 10 else 0
+ 
+# Printing value of a
+print("The value of a is: " , str(a))
+
+#Method 2
+a=5
+print("The value of a is: ",a)
